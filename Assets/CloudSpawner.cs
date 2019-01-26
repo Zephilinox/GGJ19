b5cd30220a3528f6e11fa55ee56e62cc38467e03
Unknown <thomas4.harrison@live.uwe.ac.uk>
2019-01-26 21:24:21 +0000

    Mergy Boy

---
+++ Assets/CloudSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    public GameObject cloud1;
+    public GameObject cloud2;
+    public GameObject cloud3;
+    public GameObject cloud4;
+    public GameObject cloud5;
+
+    private int cloudNumber;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+        switch(cloudNumber)
+        {
+            case 1:
+                {
+                    break;
+                }
+            case 2:
+                {
+                    break;
+                }
+            case 3:
+                {
+                    break;
+                }
+            case 4:
+                {
+                    break;
+                }
+            case 5:
+                {
+                    break;
+                }
+
+
+        }
+
+
+
+
+	}
+}

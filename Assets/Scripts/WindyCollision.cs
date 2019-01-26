462e60087a5861707da5c498d6dc523ca005757a
Zephilinox <Zephilinox@hotmail.co.uk>
2019-01-26 08:48:09 +0000

    I don't know

--- Assets/Scripts/WindyCollision.cs
+++ Assets/Scripts/WindyCollision.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class WindyCollision : MonoBehaviour
 {
+    float windPower = 0;
+    float windSpeed = 0;
+    float noiseSize = 0;
+    bool colliding = false;
+
+    private void Update()
+    {
+        GetComponent<MeshRenderer>().material.SetFloat("_Wind_Power", windPower);
+        GetComponent<MeshRenderer>().material.SetFloat("_Wind_Speed", windSpeed);
+        GetComponent<MeshRenderer>().material.SetFloat("_Noise_Size", noiseSize);
+
+        if (!colliding)
+        {
+            windPower *= 0.90f;
+            windSpeed *= 0.90f;
+            noiseSize *= 0.90f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        colliding = true;
+        windPower += 0.2f;
+        windSpeed += 0.2f;
+        noiseSize += 0.2f;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        colliding = false;
+    }
+
     private void OnCollisionStay(Collision collision)
     {
-        Debug.Log("Collision");
-        GetComponent<MeshRenderer>().material.SetFloat("_Wind_Power", 0.5f);
-        GetComponent<MeshRenderer>().material.SetFloat("_Wind_Speed", 2.5f);
-        GetComponent<MeshRenderer>().material.SetFloat("_Noise_Size", 0.5f);
+        colliding = true;
+        windPower += 0.005f;
+        windSpeed += 0.005f;
+        noiseSize += 0.005f;
     }
 }

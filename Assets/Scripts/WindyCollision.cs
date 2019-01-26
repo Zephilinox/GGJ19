41323d9ea1e6c6784000aad19243213405815559
Zephilinox <Zephilinox@hotmail.co.uk>
2019-01-26 07:57:57 +0000

    testing collision windyness

---
+++ Assets/Scripts/WindyCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindyCollision : MonoBehaviour
+{
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Collision");
+        GetComponent<MeshRenderer>().material.SetFloat("_Wind_Power", 0.5f);
+        GetComponent<MeshRenderer>().material.SetFloat("_Wind_Speed", 2.5f);
+        GetComponent<MeshRenderer>().material.SetFloat("_Noise_Size", 0.5f);
+    }
+}

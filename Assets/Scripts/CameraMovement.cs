faeb6f5fcfce044214528831b94837f59db07977
Bryce <brycenewell1998@gmail.com>
2019-01-27 12:27:30 +0000

    changed camera speed

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,6 +6,7 @@ public class CameraMovement : MonoBehaviour
 {
     public GameObject caravanModel;
     public float speed = 2.0f;
+    public float speedY = 10.0f;
     public Vector3 offset;
 
 
@@ -17,12 +18,13 @@ public class CameraMovement : MonoBehaviour
     void Update ()
     {
         float interpolation = speed * Time.deltaTime;
+        float interpolationZ = speedY * Time.deltaTime ;
 
         Vector3 position = this.transform.position;
 
         position.x = Mathf.Lerp(this.transform.position.x, caravanModel.transform.position.x + offset.x, interpolation);
         position.y = Mathf.Lerp(this.transform.position.y, caravanModel.transform.position.y + offset.y, interpolation);
-        position.z = Mathf.Lerp(this.transform.position.z, caravanModel.transform.position.z + offset.z, interpolation);
+        position.z = Mathf.Lerp(this.transform.position.z, caravanModel.transform.position.z + offset.z, interpolationZ);
 
 
         this.transform.position = position;
